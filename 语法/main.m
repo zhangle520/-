1f250c95800466685e5611e8edbfbe8eb5b47024
zhangle <zhangle@zhangledeimac.local>
2016-03-04 16:24:46 +0000

    Initial Commit

---
+++ 语法/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  语法
+//
+//  Created by zhangle on 16/3/4.
+//  Copyright © 2016年 zhangle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
